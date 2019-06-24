e27a04e499e313c4c31ee2c70b04a985d5ed0979
keybleach <diego301003@icloud.com>
2019-06-24 14:01:00 -0400

    creating clone of me me for parent class

---
+++ human.js
@@ -0,0 +1,17 @@
+function display (content){
+    console.log(content)
+}
+// define functions
+function name (diego){
+    return "hi , my name is "  + diego
+}
+
+// define parents
+display (ana.neveranswersthephone());
+display(polo.prettycool());
+
+// child class .
+// instantitat youself from the child class
+
+display(diego.videogames());
+display(diego.soccer());
