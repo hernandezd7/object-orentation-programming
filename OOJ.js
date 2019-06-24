b9445825487ed5803d122b9180dc0e50fb2fc6c9
keybleach <diego301003@icloud.com>
2019-06-24 10:00:55 -0400

    functional programming example

---
+++ OOJ.js
@@ -0,0 +1,30 @@
+// create a function that console logs any argument given 
+function display (content){
+    console.log(content);
+}
+    
+
+// create a function that takes a name or string as a parameter
+// and returns "hi my name is " parameter given 
+function name (name ){
+    return "Hi ,my name is "+ name +".";
+}
+
+
+
+// create 3 function that describe the powers of a ninja  you would like create
+
+function sharinga(ability){
+return "using "+ ability + ".";
+}
+function katon(style){
+    return "katon"+ style + "Jutsu"
+}
+function melee(move){
+    return "using my "+move +"to attack in close combat .";
+ 
+}
+display(name("itachi"));
+display (melee("punch"));
+display(katon("fire "));
+display(sharigan("mangeko"));
